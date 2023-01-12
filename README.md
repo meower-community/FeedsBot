dc2922f1467ed690e7fb99304021824c7bb0c9cc
Octo <72633854+heyimocto@users.noreply.github.com>
2023-01-12 22:25:34 +0000

    Add "unsubscribe" feature

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FeedsBot
 FeedsBot lets you subscribe to RSS and Atom feeds on Meower!
+
+
+## Functions
+- subscribe to feeds
+- recieve notifications in chat when a new article is published
+- unsubscribe from feeds
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { extract } from "@extractus/feed-extractor";
 dotenv.config();
 
 const username = process.env["FB_USERNAME"], password = process.env["FB_PASSWORD"];
-const help = [`@${username} help`, `@${username} subscribe`];
+const help = [`@${username} help`, `@${username} subscribe`, `@${username} unsubscribe`];
 const db = new JSONdb("db.json");
 const bot = new Bot(username, password);
 
@@ -50,6 +50,25 @@ bot.onPost(async (user, content) => {
             bot.post("There was a error fetching the feed!");
             return;
         }
+    }
+
+    if (content.startsWith(`@${username} unsubscribe`)) {
+        try { 
+            let feed = await extract(content.split(" ")[2]);
+            let subscriptions = db.get("feeds");
+            for (var i = subscriptions.length - 1; i >= 0; i--) {
+                if (subscriptions[i] === {"name": feed.title,"url": content.split(" ")[2], "latest": feed.entries[0],"user": user}) {
+                    subscriptions.splice(i, 1);
+                }
+            }
+        db.set(subscriptions);
+        } catch(e) {
+            console.error(e);
+            bot.post("There was a error while unsubscribing! Try again later...");
+            return;
+        }
+               
+
     }
 });
 
